3676463270b31b217b413f88f93c05dfb7429299
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-19 22:48:02 +0530

    add H2 database dependency and create initial CashCardJsonTest

---
+++ src/test/java/org/example/familycashcardapplication/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package org.example.familycashcardapplication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
