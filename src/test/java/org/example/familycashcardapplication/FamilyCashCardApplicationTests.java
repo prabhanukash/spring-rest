c266bc34a904c35969874efb0d2f45c5c9a0dece
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-20 00:11:00 +0530

    add initial SQL scripts for cash card schema and sample data

--- src/test/java/org/example/familycashcardapplication/FamilyCashCardApplicationTests.java
+++ src/test/java/org/example/familycashcardapplication/FamilyCashCardApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
