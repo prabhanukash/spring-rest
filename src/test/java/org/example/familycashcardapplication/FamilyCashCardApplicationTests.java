a8bcd07de8001c10af705fc568ba078555a54d2b
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-19 22:32:08 +0530

    initialize FamilyCashCardApplication with Gradle setup and basic structure

---
+++ src/test/java/org/example/familycashcardapplication/FamilyCashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.familycashcardapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
