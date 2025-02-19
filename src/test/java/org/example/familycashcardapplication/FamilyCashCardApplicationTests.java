67990f8deb14b964f6b99a4a57e53907d5399500
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-20 00:00:04 +0530

    add CashCardController to handle cash card retrieval by ID and update tests for validation

--- src/test/java/org/example/familycashcardapplication/FamilyCashCardApplicationTests.java
+++ src/test/java/org/example/familycashcardapplication/FamilyCashCardApplicationTests.java
@@ -1,11 +1,15 @@
 package org.example.familycashcardapplication;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,6 +24,13 @@ class FamilyCashCardApplicationTests {
         System.out.println(response);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(99);
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isNotNull();
+        assertThat(amount).isEqualTo(123.45);
     }
 
     @Test
