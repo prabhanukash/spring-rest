8adbbcb42a1b64545d98eb00910d8ff5581f07b8
bhanuprakash <prakashbhanu019@gmail.com>
2025-02-20 00:14:51 +0530

    add CashCardRepository and update CashCard for Spring Data integration

--- src/main/java/org/example/familycashcardapplication/CashCard.java
+++ src/main/java/org/example/familycashcardapplication/CashCard.java
@@ -1,5 +1,7 @@
 package org.example.familycashcardapplication;
 
-record CashCard(Long id, Double amount){
+import org.springframework.data.annotation.Id;
+
+record CashCard(@Id Long id, Double amount){
 
 }
